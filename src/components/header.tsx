6fece798d104770d15842c04730d477e65bb3a98
ronaldoapf <ronaldo.alves.1997@gmail.com>
2025-12-06 02:24:27 -0300

    feat: make avatar rounded full

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -41,7 +41,7 @@ export function Header() {
           >
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
+                <Avatar className="h-8 w-8 rounded-full">
                   <AvatarImage src={user.avatar} alt={user.name} />
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
