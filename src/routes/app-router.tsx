e4815b47698571cd6499e3822a4518fa290db3e3
ronaldoapf <ronaldo.alves.1997@gmail.com>
2025-12-07 11:31:33 -0300

    feat: login page

--- src/routes/app-router.tsx
+++ src/routes/app-router.tsx
@@ -1,12 +1,13 @@
 import { Layout } from "@/components/layout";
 import { Home } from "@/pages/home";
+import { Login } from "@/pages/login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/signin" element={<Login />} /> */}
+        <Route path="/signin" element={<Login />} />
         <Route element={<Layout />}>
           <Route path={"/"} element={<Home />} />
         </Route>
